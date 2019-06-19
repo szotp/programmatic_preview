ddc99f93c20fb8f73703880f6a025bc02f02f028
szotp <qwertyszot@gmail.com>
2019-06-19 12:16:53 -0700

    Initial Commit

---
+++ PreviewCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreviewCheck
+//
+//  Created by szotp on 19/06/2019.
+//  Copyright © 2019 szotp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
